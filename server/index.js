2601275611d23b034ebe37597b3293f6d324036f
ana scuturici <anascuturici@gmail.com>
2021-12-14 10:31:58 +0900

    Add server dir, file and dependencies

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+    res.send("Hello");
+});
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+})
