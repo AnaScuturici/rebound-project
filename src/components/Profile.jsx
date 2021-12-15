ff1874881b5918f29773cd672417463363b3b8e8
ana scuturici <anascuturici@gmail.com>
2021-12-15 15:15:32 +0900

    Fix profile description

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -17,13 +17,15 @@ function Profile() {
     <div className="profile-page">
       <img className="profile-pic" alt="profile-pic" src={photo} ></img>
       <h1>Julia Ivan</h1>
-      <p className="description">Julia is a licensed Family Therapist in London.{"\n"} She has been working in the mental health field for over ten years, {"\n"} and has experience working with adults, children and families who have experienced trauma.</p>
+      <p className="description">Julia is a licensed Family Therapist in London.<br></br>
+      She has been working in the mental health field for over ten years, <br></br> 
+      and has experience working with adults, children and families who have experienced trauma.</p>
       <h3>Specialized in:</h3>
       <p>Family Therapy, Counseling and Psychotherapy</p>
       <button className="book-btn" onClick={handleClick}>Book a session with me!</button>
       {view && (
         <>
-        <p>Pick a date and send me a message about what you would like to discuss with me. {"\n"}
+        <p>Pick a date and send me a message about what you would like to discuss with me. <br></br>
         I will get back to you within a day to confirm our appointment!
       </p>
         <Calendar onChange={setDate} value={date} />
