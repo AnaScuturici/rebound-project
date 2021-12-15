de8b00ce704c891fcac215288203fc91dbaa5a0d
ana scuturici <anascuturici@gmail.com>
2021-12-15 11:00:22 +0900

    Refactor conditional rendering for calendar and contact form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import Profile from "./components/Profile";
-import ContactForm from "./components/ContactForm";
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
+import React from 'react';
 import Navbar from "./components/Navbar";
-import "react-calendar/dist/Calendar.css";
 
 function App() {
-  const [date, setDate] = useState(new Date());
-
   return (
     <div className="App">
       <Navbar />
       <div className="components">
         <Profile />
-        <Calendar onChange={setDate} value={date} />
-        <ContactForm date={date.toDateString()} />
       </div>
     </div>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,18 +1,32 @@
 import photo from "./profile-pic.jpeg";
 import "./Profile.css";
+import ContactForm from "./ContactForm";
+import Calendar from 'react-calendar';
+import "react-calendar/dist/Calendar.css";
+import React, { useState } from 'react';
 
 function Profile() {
-    return (
-      <div className="profile-page">
-       <header>
-       <img className="profile-pic" alt="profile-pic" src={photo} ></img>
-       <h1>Welcome!</h1>
-       <p>About me</p>
-       <button className="book-btn">Book a session with me!</button>
-       </header>
-      </div>
-    );
-  }
-  
-  export default Profile;
-  
+  const [date, setDate] = useState(new Date());
+  const [view, setView] = useState(false);
+
+  const handleClick = () => {
+    setView(true);
+  };
+
+  return (
+    <div className="profile-page">
+      <img className="profile-pic" alt="profile-pic" src={photo} ></img>
+      <h1>Welcome!</h1>
+      <p>About me</p>
+      <button className="book-btn" onClick={handleClick}>Book a session with me!</button>
+      {view && (
+        <>
+        <Calendar onChange={setDate} value={date} />
+        <ContactForm date={date.toDateString()} />
+      </>
+      )}
+    </div>
+  )
+}
+
+export default Profile;
